e2c3b951b2e742a0570ebce9802765a5b9a25180
folakemi10 <71245496+folakemi10@users.noreply.github.com>
2024-03-20 23:49:54 -0500

    finally done
    finally done

--- Assets/Code/Q5/Ship.cs
+++ Assets/Code/Q5/Ship.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SpaceShooter
 {
     public class Ship : MonoBehaviour
     {
         public GameObject projectilePrefab;
+        public Image imageHealthBar;
 
         public float firingDelay = 1f;
+        public float healthMax = 100f;
+        public float health = 100f;
         // Start is called before the first frame update
         void Start()
         {
@@ -19,11 +24,25 @@ namespace SpaceShooter
         // Update is called once per frame
         void Update()
         {
-            float yPosition = Mathf.Sin(GameController.instance.timeElapsed) * 3f;
-            transform.position = new Vector2(0, yPosition);
-
+            if (health > 0)
+            {
+                float yPosition = Mathf.Sin(GameController.instance.timeElapsed) * 3f;
+                transform.position = new Vector2(0, yPosition);
+            }
         }
 
+        void Die()
+        {
+            StopCoroutine("FiringTimer");
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            
+        }
         void FireProjectile()
         {
             Instantiate(projectilePrefab, transform.position, Quaternion.identity);
